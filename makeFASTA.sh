2c8602edb003ba2543dc0fa81c8fb6e58ff7d132
Parnika <parnika91@gmail.com>
2021-02-05 17:57:17 +0100

    added script description

--- makeFASTA.sh
+++ makeFASTA.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sh
 
-# host and oarasite combinations currently in use 
+# to show how host-parasite pair genome sequences files 
+# were concatenated to prevent >100% mapping
 
 cat human.fa Pfalciparum.fa > humanPfalciparum.fa
 cat human.fa Pberghei.fa > humanPberghei.fa
